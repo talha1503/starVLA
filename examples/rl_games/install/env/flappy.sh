76dc7594402377648a2974e33804bd0e88f30cfd
Talha Chafekar <talha1503@gmail.com>
2026-05-20 05:52:55 -0400

    pygame sprite fix

--- examples/rl_games/install/env/flappy.sh
+++ examples/rl_games/install/env/flappy.sh
@@ -9,8 +9,30 @@ PYTHON_BIN="${PYTHON_BIN:-python}"
 import os
 os.environ.setdefault("SDL_VIDEODRIVER", "dummy")
 
+from pathlib import Path
+
 import flappy_bird_gymnasium  # noqa: F401
+import flappy_bird_gymnasium.envs.utils as flappy_utils
 import gymnasium as gym
+import pygame
+from PIL import Image
+
+repaired = []
+for sprite_path in sorted(Path(flappy_utils.SPRITES_PATH).glob("*.png")):
+    try:
+        pygame.image.load(str(sprite_path))
+        continue
+    except pygame.error:
+        pass
+
+    image = Image.open(sprite_path)
+    mode = "RGBA" if image.mode in {"RGBA", "LA", "P"} else "RGB"
+    image.convert(mode).save(sprite_path, format="PNG", optimize=False)
+    pygame.image.load(str(sprite_path))
+    repaired.append(sprite_path.name)
+
+if repaired:
+    print(f"[install/env/flappy] repaired sprite PNGs: {', '.join(repaired)}")
 
 env = gym.make("FlappyBird-v0", render_mode="rgb_array", use_lidar=False)
 env.reset()
