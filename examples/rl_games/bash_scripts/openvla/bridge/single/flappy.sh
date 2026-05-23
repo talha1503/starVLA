aff4f5c882fce51ef32b208ddc9e7417eab0154c
Talha Chafekar <talha1503@gmail.com>
2026-05-23 06:44:26 +0000

    flappy openvla base config

---
+++ examples/rl_games/bash_scripts/openvla/bridge/single/flappy.sh
@@ -0,0 +1,28 @@
+cd starVLA
+
+bash examples/rl_games/install/install_stack.sh openvla flappy
+
+conda activate starvla_rl_games_openvla
+
+bash examples/rl_games/scripts/run_experiment.sh \
+    examples/rl_games/experiments/openvla/bridge/single/flappy.yaml \
+    run_id="openvla_bridge_flappy_single_latency_exp1" \
+    trainer.distributed_backend=none \
+    workspace="/workspace" \
+    wandb.entity="talha1503" \
+    checkpoint.hf_repo_id="talha1503/openvla_bridge_flappy_single_latency_exp1" \
+    checkpoint.sync_enabled=true \
+    checkpoint.sync_repo_id="talha1503/openvla_bridge_flappy_single_latency_exp1" \
+    trainer.max_train_steps=5000 \
+    trainer.num_warmup_steps=0 \
+    trainer.save_interval=10 \
+    trainer.eval_interval=500 \
+    trainer.logging_frequency=1 \
+    trainer.gradient_accumulation_steps=16 \
+    trainer.batch_size=16 \
+    mid_train_eval.interval_steps=500 \
+    mid_train_eval.max_steps_per_episode=3600 \
+    post_train_eval.enabled=true \
+    post_train_eval.latencies=[0,1,2,3,4,5,6,7] \
+    post_train_eval.num_episodes=20 \
+    post_train_eval.max_steps_per_episode=3600
