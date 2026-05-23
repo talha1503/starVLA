9a85593fe55aec12be26f25e7ee8c66b9ef824d0
Talha Chafekar <talha1503@gmail.com>
2026-05-23 06:03:08 -0400

    fix config args

--- examples/rl_games/bash_scripts/openvla/bridge/single/deadly_corridor.sh
+++ examples/rl_games/bash_scripts/openvla/bridge/single/deadly_corridor.sh
@@ -1,18 +1,18 @@
 cd starVLA
 
-bash examples/rl_games/install/install_stack.sh openvla flappy
+bash examples/rl_games/install/install_stack.sh openvla deadly_corridor
 
 conda activate starvla_rl_games_openvla
 
 bash examples/rl_games/scripts/run_experiment.sh \
-    examples/rl_games/experiments/openvla/bridge/single/flappy.yaml \
-    run_id="openvla_bridge_flappy_single_latency_exp1" \
+    examples/rl_games/experiments/openvla/bridge/single/deadly_corridor.yaml \
+    run_id="openvla_bridge_deadly_corridor_single_latency_exp1" \
     trainer.distributed_backend=none \
     workspace_dir="/workspace" \
     wandb.entity="talha1503" \
-    checkpoint.hf_repo_id="talha1503/openvla_bridge_flappy_single_latency_exp1" \
+    checkpoint.hf_repo_id="talha1503/openvla_bridge_deadly_corridor_single_latency_exp1" \
     checkpoint.sync_enabled=true \
-    checkpoint.sync_repo_id="talha1503/openvla_bridge_flappy_single_latency_exp1" \
+    checkpoint.sync_repo_id="talha1503/openvla_bridge_deadly_corridor_single_latency_exp1" \
     checkpoint.local_keep_last_n=2 \
     trainer.max_train_steps=5000 \
     trainer.num_warmup_steps=0 \
@@ -25,6 +25,6 @@ bash examples/rl_games/scripts/run_experiment.sh \
     rl_games.mid_train_eval.max_steps_per_episode=3600 \
     rl_games.mid_train_eval.num_episodes=20 \
     rl_games.post_train_eval.enabled=true \
-    rl_games.post_train_eval.latencies=[0,1,2,3,4,5,6,7] \
+    rl_games.post_train_eval.latencies=[0,1,2,3,4,5,6,7,8,9,10] \
     rl_games.post_train_eval.num_episodes=20 \
     rl_games.post_train_eval.max_steps_per_episode=3600
