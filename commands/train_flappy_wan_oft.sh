f6540faab09535a1a85a51a0f8b8437d109be2ab
dongqianyu99 <dongqianyu99@gmail.com>
2026-06-22 19:18:58 +0800

    chore: raise WanOFT flappy batch size

--- commands/train_flappy_wan_oft.sh
+++ commands/train_flappy_wan_oft.sh
@@ -13,7 +13,7 @@ python examples/rl_games/scripts/launch_train.py \
   paths.dataset_local_dir=data/flappy_fix_latency_0_200ep_context4 \
   trainer.distributed_backend=none \
   trainer.gradient_accumulation_steps=16 \
-  datasets.vla_data.per_device_batch_size=1 \
+  datasets.vla_data.per_device_batch_size=8 \
   trainer.max_train_steps=5000 \
   trainer.save_interval=100 \
   rl_games.env_eval.mid_train.enabled=false \
