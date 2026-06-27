8bb7aaa81e380377acc709bc64e134b7500b42d7
Xinyuan Li <xinyuanli2327@outlook.com>
2026-06-28 00:46:47 +0800

    fix: turn off checkpointing for kv memory, cause cache=True will conflict

--- commands/memory/train_flappy_openvla_kv_memory.sh
+++ commands/memory/train_flappy_openvla_kv_memory.sh
@@ -32,6 +32,7 @@ python examples/rl_games/scripts/launch_train.py \
   datasets.vla_data.num_obs_frames=8 \
   datasets.vla_data.image_mode=multiframe \
   framework.qwenvl.attn_implementation=flash_attention_2 \
+  framework.qwenvl.enable_gradient_checkpointing=false \
   framework.kv_memory.enabled=true \
   framework.kv_memory.window=4 \
   framework.kv_memory.rollout_len=8 \
