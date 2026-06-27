9aa6cfc54492d12510c643296dc3a6c3af953da0
Xinyuan Li <xinyuanli2327@outlook.com>
2026-06-28 02:46:28 +0800

    fix(dataloader): isolate worker CUDA and distributed env

--- starVLA/dataloader/cpu_only_worker_dataloader.py
+++ starVLA/dataloader/cpu_only_worker_dataloader.py
@@ -4,13 +4,32 @@ import multiprocessing.context
 from torch.utils.data import DataLoader
 
 
+_DISTRIBUTED_ENV_KEYS = (
+    "LOCAL_RANK",
+    "RANK",
+    "WORLD_SIZE",
+    "MASTER_ADDR",
+    "MASTER_PORT",
+    "GROUP_RANK",
+    "LOCAL_WORLD_SIZE",
+    "ROLE_RANK",
+    "ROLE_WORLD_SIZE",
+    "TORCHELASTIC_RUN_ID",
+    "TORCHELASTIC_RESTART_COUNT",
+    "TORCHELASTIC_MAX_RESTARTS",
+)
+
+
 class _CpuOnlyWorkerSpawnProcess(multiprocessing.context.SpawnProcess):
     def start(self):
         had_cuda_visible_devices = "CUDA_VISIBLE_DEVICES" in os.environ
         if had_cuda_visible_devices:
             cuda_visible_devices = os.environ["CUDA_VISIBLE_DEVICES"]
+        distributed_env = {key: os.environ[key] for key in _DISTRIBUTED_ENV_KEYS if key in os.environ}
 
         os.environ["CUDA_VISIBLE_DEVICES"] = ""
+        for key in distributed_env:
+            del os.environ[key]
         try:
             return super().start()
         finally:
@@ -18,6 +37,8 @@ class _CpuOnlyWorkerSpawnProcess(multiprocessing.context.SpawnProcess):
                 os.environ["CUDA_VISIBLE_DEVICES"] = cuda_visible_devices
             else:
                 del os.environ["CUDA_VISIBLE_DEVICES"]
+            for key, value in distributed_env.items():
+                os.environ[key] = value
 
 
 class CpuOnlyWorkerSpawnContext(multiprocessing.context.SpawnContext):
--- tests/test_cpu_only_worker_dataloader.py
+++ tests/test_cpu_only_worker_dataloader.py
@@ -8,12 +8,39 @@ from torch.utils.data import Dataset
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 
+_DISTRIBUTED_ENV_KEYS = (
+    "LOCAL_RANK",
+    "RANK",
+    "WORLD_SIZE",
+    "MASTER_ADDR",
+    "MASTER_PORT",
+    "GROUP_RANK",
+    "LOCAL_WORLD_SIZE",
+    "ROLE_RANK",
+    "ROLE_WORLD_SIZE",
+    "TORCHELASTIC_RUN_ID",
+    "TORCHELASTIC_RESTART_COUNT",
+    "TORCHELASTIC_MAX_RESTARTS",
+)
+
+
 class _CudaVisibleDevicesDataset(Dataset):
     def __len__(self):
         return 1
 
     def __getitem__(self, index):
         return os.environ["CUDA_VISIBLE_DEVICES"]
+
+
+class _WorkerEnvDataset(Dataset):
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, index):
+        return {
+            "CUDA_VISIBLE_DEVICES": os.environ["CUDA_VISIBLE_DEVICES"],
+            **{key: os.environ.get(key, "<unset>") for key in _DISTRIBUTED_ENV_KEYS},
+        }
 
 
 def test_worker_inherits_empty_cuda_visible_devices_and_parent_is_restored(monkeypatch):
@@ -67,3 +94,26 @@ def test_cpu_only_worker_context_survives_dataloader_reconstruction(monkeypatch)
 
     assert next(iter(reconstructed)) == [""]
     assert os.environ["CUDA_VISIBLE_DEVICES"] == "0,1"
+
+
+def test_worker_does_not_inherit_distributed_environment(monkeypatch):
+    from starVLA.dataloader.cpu_only_worker_dataloader import CpuOnlyWorkerDataLoader
+
+    monkeypatch.setenv("CUDA_VISIBLE_DEVICES", "0,1")
+    for index, key in enumerate(_DISTRIBUTED_ENV_KEYS):
+        monkeypatch.setenv(key, f"value-{index}")
+
+    loader = CpuOnlyWorkerDataLoader(
+        _WorkerEnvDataset(),
+        batch_size=1,
+        num_workers=1,
+    )
+    worker_env = next(iter(loader))
+
+    assert worker_env["CUDA_VISIBLE_DEVICES"] == [""]
+    for key in _DISTRIBUTED_ENV_KEYS:
+        assert worker_env[key] == ["<unset>"]
+
+    assert os.environ["CUDA_VISIBLE_DEVICES"] == "0,1"
+    for index, key in enumerate(_DISTRIBUTED_ENV_KEYS):
+        assert os.environ[key] == f"value-{index}"
