77e9890f8bd1dd0ecf3efe4030f311d807322d8e
Talha Chafekar <talha1503@gmail.com>
2026-07-28 18:27:19 -0400

    add final ghost trail scripts:

---
+++ examples/rl_games/bash_scripts/openvla/bridge/ghost_trail/final_flappy_latency_4_ablation_memory.sh
@@ -0,0 +1,50 @@
+export WORKSPACE_DIR
+
+bash "${WORKSPACE_DIR}/starVLA/examples/rl_games/bash_scripts/install/pre_launch.sh"
+
+cd "${WORKSPACE_DIR}/starVLA"
+
+bash examples/rl_games/install/install_stack.sh openvla flappy
+
+conda activate starvla_rl_games_openvla
+
+bash "${WORKSPACE_DIR}/starVLA/examples/rl_games/bash_scripts/install/latency_deps.sh"
+
+export PYTHONPATH="${WORKSPACE_DIR}/latency-sensitive-bench:${PYTHONPATH:-}"
+
+python examples/rl_games/scripts/launch_train.py \
+    model=openvla \
+    env=flappy \
+    init=bridge \
+    mode=single \
+    run_id="openvla_bridge_flappy_fixed_latency_3_200ep_7k2steps_ghost15" \
+    trainer.distributed_backend=none \
+    workspace_dir="$WORKSPACE_DIR" \
+    wandb_entity="talha1503" \
+    checkpoint.hf_repo_id="talha15032/openvla_bridge_flappy_fixed_latency_3_200ep_7k2steps_ghost15" \
+    checkpoint.sync.enabled=true \
+    checkpoint.sync.repo_id="talha15032/openvla_bridge_flappy_fixed_latency_3_200ep_7k2steps_ghost15" \
+    dataset.source_hf="latency-sensitive-bench/flappy_fixed_latency_3_200ep_7k2steps_ghost15" \
+    dataset.latency_filter=[3] \
+    datasets.vla_data.sequential_step_sampling=true \
+    datasets.vla_data.shuffle=true \
+    trainer.per_latency_eval_num_batches=5 \
+    dataset.episodes_per_latency=200 \
+    rl_games.env_eval.image_transform=flappy_ghost_trail \
+    rl_games.env_eval.post_train.ghost_trail.history_frames=15 \
+    rl_games.env_eval.eval_backend=eval_core \
+    datasets.vla_data.shuffle=true \
+    checkpoint.local.keep_last_n=1 \
+    trainer.max_train_steps=5000 \
+    trainer.num_warmup_steps=0 \
+    checkpoint.save_final_model=true \
+    checkpoint.save_best_model=false \
+    trainer.eval_interval=5000 \
+    trainer.logging_frequency=1 \
+    trainer.gradient_accumulation_steps=1 \
+    datasets.vla_data.per_device_batch_size=32 \
+    rl_games.env_eval.mid_train.enabled=false \
+    rl_games.env_eval.post_train.enabled=true \
+    rl_games.env_eval.post_train.latencies=[3] \
+    rl_games.env_eval.post_train.num_episodes=20 \
+    rl_games.env_eval.post_train.max_steps_per_episode=3600
